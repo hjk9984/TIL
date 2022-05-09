5ac8d8f0c958c66c4d70fec474d32bd0f0b8f7e0
hjk9984 <hjk9984@naver.com>
2022-05-09 15:09:39 +0900

    Update pytorch autograd  detach, no_grad

--- python/pytorch/autograd.py
+++ python/pytorch/autograd.py
@@ -9,7 +9,8 @@ import torch.nn.functional as F
 x = torch.ones(5)
 y = torch.zeros(3)
 w = torch.randn(5, 3, requires_grad=True)   # >> param
-b = torch.randn(3, requires_grad=True)      # >> param
+b = torch.randn(3)      # >> param
+b.requires_grad_(True)
 z = torch.matmul(x, w) + b
 loss = F.binary_cross_entropy_with_logits(z, y)
 
@@ -22,3 +23,19 @@ print(f"gradient func for loss = {loss.grad_fn}")
 loss.backward()
 print(w.grad)
 print(b.grad)
+
+# no_grad temporarily set all the requires_grad flag to false
+# it will use less memory because it knows from the beginning 
+# that no gradients are needed so it doesn't need to keep intermediary results
+with torch.no_grad():
+    z = torch.matmul(x, w) + b
+print(f"torch.no_grad = {z.requires_grad}")
+
+# detach creates a new tensor that shares storage with the tensor
+# that does not require grad.
+# it detaches the output from the computational graph.
+z = torch.matmul(x, w) + b
+print(z.requires_grad)
+z_det = z.detach()
+print(z.requires_grad)
+print(z_det.requires_grad)
